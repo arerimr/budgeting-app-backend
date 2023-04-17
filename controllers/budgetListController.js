bbf8560ca337b3a8f48f2a2ebd14f47957734dbc
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 19:53:44 -0400

    get one item

--- controllers/budgetListController.js
+++ controllers/budgetListController.js
@@ -4,8 +4,14 @@ const budget = express.Router()
 
 const budgetListArr = require('../models/budgetList.js')
 
-budget.get("/", (req, res)=>{
+budget.get("/", (req, res) => {
     res.status(200).send(budgetListArr)
+})
+
+budget.get("/:id", (req, res) => {
+    const { id } = req.params
+    const item = budgetListArr[id]
+    item ? res.status(200).send(item) : res.status(200).redirect("/")
 })
 
 module.exports = budget
