785b5035317ac2ac871f3d49f8d849a0e7126af3
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 19:25:13 -0400

    added mock data

---
+++ models/budgetList.js
@@ -0,0 +1,93 @@
+module.exports = [
+    {
+       date: "04/01/2023",
+       title: "Rent",
+       amount: 1250,
+       comment: "Gotta pay it on the 1st",
+       priority: "high",
+       completed: true,
+       income: false
+    },
+    {
+        date: "04/20/2023",
+        title: "Car Note",
+        amount: 200,
+        comment: "Zelle money to mom",
+        priority: "medium",
+        completed: false,
+        income: false
+     },
+     {
+        date: "04/19/2023",
+        title: "Paycheck",
+        amount: 598,
+        comment: "shhmoneyy coming in finally",
+        priority: "high",
+        completed: false,
+        income: true
+     },
+     {
+        date: "04/17/2023",
+        title: "Uber Eats",
+        amount: 85,
+        comment: "pay credit card",
+        priority: "medium",
+        completed: true,
+        income: true
+     },
+     {
+        date: "04/26/2023",
+        title: "Paycheck",
+        amount: 599,
+        comment: "shhmoneyy coming in finally",
+        priority: "high",
+        completed: false,
+        income: true
+     },
+     {
+        date: "04/12/2023",
+        title: "Paycheck",
+        amount: 620,
+        comment: "shhmoneyy coming in finally",
+        priority: "high",
+        completed: true,
+        income: true
+     },
+     {
+        date: "04/05/2023",
+        title: "Paycheck",
+        amount: 598,
+        comment: "shhmoneyy coming in finally",
+        priority: "high",
+        completed: true,
+        income: true
+     },
+     {
+        date: "04/22/2023",
+        title: "Wifi",
+        amount: 24,
+        comment: "pay with apple card, then pay the card",
+        priority: "medium",
+        completed: false,
+        income: false
+     },
+     {
+        date: "04/30/2023",
+        title: "Tote bag",
+        amount: 200,
+        comment: "Buy when you have money",
+        priority: "low",
+        completed: false,
+        income: false
+     },
+     {
+        date: "04/30/2023",
+        title: "diesmo",
+        amount: 200,
+        comment: "God's money",
+        priority: "high",
+        completed: false,
+        income: false
+     },
+
+]
