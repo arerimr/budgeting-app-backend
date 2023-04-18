6a882c28838aee95df68586f2b6265f302ebb56e
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 20:36:48 -0400

    added project requirements

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ app.use(express.json())
 
 const budgetListController = require('./controllers/budgetListController.js')
 
-app.use('/list', budgetListController)
+app.use('/transactions', budgetListController)
 
 app.get("/", (req, res) => {
     res.send("hello hello")
 })
 
+app.get("*", (req, res) => {
+    res.status(404).send("page/log does not exist")
+})
 
 module.exports = app
