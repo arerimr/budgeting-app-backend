5869707b45e8ea912fc806e83d0de657089bb8b0
arerimr <raydelys.morrobelr@gmail.com>
2023-04-17 21:50:11 -0400

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+const cors = require("cors")
+
+app.use(cors())
+
 app.use(express.json())
 
 const budgetListController = require('./controllers/budgetListController.js')
